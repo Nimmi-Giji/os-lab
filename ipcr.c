683f9e5a151b414db3237e85238bb3bccb73fb4b
Nimmi-Giji <134776783+Nimmi-Giji@users.noreply.github.com>
2024-04-29 18:57:11 +0530

    ipc

--- ipcr.c
+++ ipcr.c
@@ -3,4 +3,32 @@
 #include<unistd.h>
 #include<sys/ipc.h>
 #include<sys/shm.h>
-//reader
+#include<string.h>
+
+#define shm 32
+
+int main() {
+    int shmid;
+    char* shmaddr;
+    key_t key = 1234;
+
+    if((shmid = shmget(key, shm, 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
+
+    if((shmaddr = shmat(shmid, NULL, 0)) == (char *) -1){
+        perror("shmat");
+        exit(1);
+    }
+
+    while(shmaddr[shm - 1]!='!') sleep(1);
+
+    printf("Data read %s\n", shmaddr);
+
+    shmaddr[shm - 1] = '*';
+
+    shmdt(shmaddr);
+    
+    return 0;
+}
--- ipcw.c
+++ ipcw.c
@@ -3,4 +3,35 @@
 #include<unistd.h>
 #include<sys/ipc.h>
 #include<sys/shm.h>
-//writer
+#include<string.h>
+
+#define shm 32
+
+int main() {
+    int shmid;
+    char* shmaddr;
+    key_t key = 1234;
+
+    if((shmid = shmget(key, shm, IPC_CREAT|0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
+
+    if((shmaddr = shmat(shmid, NULL, 0)) == (char *)-1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    printf("Enter a string: ");
+    fgets(shmaddr, shm, stdin);
+
+    shmaddr[shm - 1] = '!';
+    printf("Data written!");
+
+    while(shmaddr[shm - 1]!='*') sleep(1);
+
+    shmdt(shmaddr);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    return 0;
+}
